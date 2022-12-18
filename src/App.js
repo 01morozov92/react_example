efec0ef6a0c0b78246c0491613311b1868ff8798
01morozov92 <01morozov92@gmail.com>
2022-12-18 18:10:57 +0600

    input > postList and remove and sort

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import "./styles/AppTest.css"
-import PostItem from "./components/PostItem";
 import {useState} from "react";
 import PostList from "./components/PostList";
 import MyForm from "./components/MyForm";
+import MySelect from "./components/ui/select/MySelect";
 
 function App() {
 
     const [posts, setPosts] = useState([
-        {id:1, title:"Java", body:"Streams api"},
-        {id:2, title:"Java", body:"Streams api"},
-        {id:3, title:"Java", body:"Streams api"},
-        ])
+        {id: 1, title: "AB", body: "BA"},
+        {id: 2, title: "BA", body: "CA"},
+        {id: 3, title: "CA", body: "AB"},
+    ])
+
+    const [selectedSort, setSelectedSort] = useState("")
+
+    const sortPosts = (sort) => {
+        setSelectedSort(sort)
+        setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+    }
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -23,6 +30,15 @@ function App() {
     return (
         <div className="App">
             <MyForm create={createPost}/>
+            <MySelect
+                defaultOption="Sort by"
+                value={selectedSort}
+                onChange={sortPosts}
+                options={[
+                    {value: "title", name: "By title"},
+                    {value: "body", name: "By description"}
+                ]}
+            />
             <PostList remove={deletePost} title="Posts" posts={posts}/>
         </div>
     );
