e3e6e25bfd50b3741faacd06c4271e65487d203f
01morozov92 <01morozov92@gmail.com>
2022-12-30 22:30:04 +0600

    complete route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import "./styles/App.css"
-import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import About from "./pages/About";
 import Posts from "./pages/Posts";
-import Navbar from "./pages/Navbar";
 import NotFound from "./pages/NotFound";
 import MainPage from "./pages/MainPage";
 
